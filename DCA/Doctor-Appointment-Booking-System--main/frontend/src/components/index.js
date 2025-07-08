9b44debf1f80fc4ff52bd0347c5ac914380ca882
Yousefmm09 <yousefmohsensm@gmail.com>
2025-07-08 22:26:37 +0300

    Remove components folder

--- DCA/Doctor-Appointment-Booking-System--main/frontend/src/components/index.js
+++
@@ -1,6 +0,0 @@
-// Export all components from this file
-export { default as PaymentModal } from './PaymentModal';
-export { default as PaymentHistory } from './PaymentHistory';
-export { default as AppointmentPaymentStatus } from './AppointmentPaymentStatus';
-
-// Add other component exports as needed 
